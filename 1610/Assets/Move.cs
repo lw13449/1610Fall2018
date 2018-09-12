a7af5cb6edf8e1e2941555022796c8f5217c9f41
Lillian Wright <wright_lillian@rocketmail.com>
2018-09-12 15:34:09 -0600

    Commit Floats and Components

---
+++ 1610/Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public CharacterController Character;
+	public float Speed;
+	
+	public Vector3 MoveSpeed;
+	public Vector3 RotateSpeed;
+	// Update is called once per frame
+	void Update ()
+	{
+		MoveSpeed.x = Speed*Input.GetAxis("Horizontal");
+		MoveSpeed *= Time.deltaTime;
+
+		Character.Move(MoveSpeed);
+	}
+}

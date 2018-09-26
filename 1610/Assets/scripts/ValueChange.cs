92938e821147a4018b17ea997b372d0281268c19
Lillian Wright <wright_lillian@rocketmail.com>
2018-09-26 15:45:56 -0600

    Mouse behaviors and Power up script

---
+++ 1610/Assets/scripts/ValueChange.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ValueChange : ScriptableObject
+{
+
+    public FloatData Data;
+
+    public void AddValue(FloatData data)
+    {
+        data.Value += FloatData.Value;
+    }
+
+    public void SubtractValue(FloatData data)
+    {
+        data.Value -= FloatData.Value;
+    }
+
+    public void MultiplyValue(FloatData data)
+    {
+        data.Value *= FloatData.Value;
+    }
+
+    public void DivideValue(FloatData)
+    {
+        FloatData.Value /= FloatData.Value;
+    }
+}

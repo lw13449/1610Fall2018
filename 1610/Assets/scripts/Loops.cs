455666c454fa1688160fe02b3579b5044395021a
Lillian Wright <wright_lillian@rocketmail.com>
2018-10-24 15:06:11 -0600

    Loop Data

---
+++ 1610/Assets/scripts/Loops.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+
+	public string[] Names;
+	public GameObject[] Things;
+	
+	void Start () {
+		for (int i = 0; i < Names.Length; i++)
+		{
+			print(Names[i]);
+		}
+
+		foreach (var thing in Things)
+		{
+			print(thing);
+		}
+	}
+	
+	
+	void Update () {
+		
+	}
+}

64693d71a7a02b5d8421f36acd220302bd586d3a
Lillian Wright <wright_lillian@rocketmail.com>
2018-11-05 14:58:41 -0700

    AI scene

---
+++ 1610/Assets/scripts/AI/BasicAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicAI : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	public Transform Destination;
+	
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+	{
+		agent.destination = Destination.position;
+	}
+}

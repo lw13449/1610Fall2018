248645fda68fb948eb3426c1754e397a928fe7da
Lillian Wright <wright_lillian@rocketmail.com>
2018-11-14 14:40:45 -0700

    For Each Loops

---
+++ 1610/Assets/scripts/ForEachLoo[s/LetterInstanceLoop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterInstanceLoop : MonoBehaviour
+{
+
+	public GameObject[] Word;
+	private Vector3 position;
+
+	private void Start()
+	{
+		foreach (var letter in Word)
+		{
+			position.x += 2.5f;
+			Instantiate(letter, position, Quaternion.identity);
+		}
+	}
+}
---
+++ 1610/Assets/scripts/ForEachLoo[s/LetterLoop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterLoop : MonoBehaviour
+{
+
+	public string[] Alphabet;
+	
+	// Use this for initialization
+	void Start () {
+		foreach (var letter in Alphabet)
+		{
+			print(letter);
+		}
+	}
+	
+}

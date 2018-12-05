0fa29d9219cf1b262aa0758858135724814db7b6
Unknown <wright_lillian@rocketmail.com>
2018-12-04 19:14:02 -0700

    final game update

---
+++ Unity/Fall 1610 HomePractice/Assets/FinalGame/Scripts/DestroyObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+	IEnumerator Start()
+	{
+	    yield return new WaitForSeconds(2);
+	    Destroy(gameObject);
+	}
+	
+	private void OnCollisionStay(Collision other)
+	{
+	    Destroy(other.gameObject);
+	    Destroy(gameObject);
+	}
+	
+}

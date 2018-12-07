ff1eb41634b0631aa33b987cfda84e7028d3bbd2
Unknown <wright_lillian@rocketmail.com>
2018-12-07 11:55:13 -0700

    final game update

---
+++ Unity/Fall 1610 HomePractice/Assets/FinalGame/Scripts/PowerUps/Change Value/ValueChange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ValueChange : ScriptableObject
+{
+	public FloatData Data;
+
+	public void AddValue(FloatData data)
+	{
+		data.Value += Data.Value;
+	}
+
+	public void SubtractValue(FloatData data)
+	{
+		data.Value -= Data.Value;
+	}
+	
+}

7a7d1ba482a2663b96d5b59a23afb3a0da6fbf3f
Unknown <wright_lillian@rocketmail.com>
2019-04-05 22:10:51 -0600

    practice update

---
+++ Unity/Fall 1610 HomePractice/Assets/Scripts/IntVariable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntVariable : MonoBehaviour {
+
+	static void Main(string[] args)
+	{
+	    int x = 5;
+	    int y = 145 + x;
+	    char p = 'p';
+	    p = 'A';
+	 }
+}
